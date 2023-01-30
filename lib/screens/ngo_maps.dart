e6a4a3d6389b07d17811a622426fb39f854fd450
Rohit Ranjan <ranjanrohit812@gmail.com>
2023-01-30 15:02:10 +0530

    Bottom Nav Bar

---
+++ lib/screens/ngo_maps.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class NgoMaps extends StatelessWidget {
+  const NgoMaps({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('NGO Maps'),
+      ),
+    );
+  }
+}

e14be5535bc961d4753c2e2f752ca4862c48f019
Yash Satankar <yashsatankar21@gmail.com>
2023-02-15 14:57:39 +0530

    response

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,9 +1,13 @@
+import 'dart:ui';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:decarbonus/screens/question_welcome.dart';
 import 'package:decarbonus/utils/get_blogs.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:syncfusion_flutter_charts/charts.dart';
 
 import '../models/fade_animation.dart';
 
@@ -17,42 +21,42 @@ class HomePage extends ConsumerStatefulWidget {
 class _HomePageState extends ConsumerState<HomePage> {
   @override
   Widget build(BuildContext context) {
-     getBlogs(ref, context);
+    final List<ChartData> chartData = [
+      ChartData('David', 25),
+      ChartData('Steve', 38),
+      ChartData('Jack', 34),
+      ChartData('Others', 52)
+    ];
     return Scaffold(
       body: FadeAnimation(
         delay: 0,
         fadeDirection: FadeDirection.right,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Center(
-              child: Text('Dashboard'),
-            ),
-            SizedBox(
-              height: MediaQuery.of(context).size.height * 0.02,
-            ),
-            ElevatedButton(
-              onPressed: () {
-                FirebaseFirestore.instance
-                    .collection('users')
-                    .doc(FirebaseAuth.instance.currentUser!.uid)
-                    .update({
-                  'isResponded': false,
-                  'responses': {},
-                  'results': {},
-                });
-                Navigator.pushReplacement(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => const QuestionWelcomeScreen(),
-                  ),
-                );
-              },
-              child: const Text('Clear Responses'),
-            )
-          ],
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Container(
+                height: MediaQuery.of(context).size.height * 0.55,
+                decoration: BoxDecoration(
+                  color: Colors.teal.shade200,
+                ),
+                child: SfCircularChart(series: <CircularSeries>[
+                  // Renders radial bar chart
+                  RadialBarSeries<ChartData, String>(
+                      dataSource: chartData,
+                      xValueMapper: (ChartData data, _) => data.x,
+                      yValueMapper: (ChartData data, _) => data.y)
+                ]),
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
+}
+
+class ChartData {
+  ChartData(this.x, this.y);
+  final String x;
+  final double y;
 }

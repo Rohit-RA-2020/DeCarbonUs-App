cc040edfe975a81e34b1e9b350261f7651ab5a39
Yash Satankar <yashsatankar21@gmail.com>
2023-02-15 21:45:42 +0530

    dash

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:syncfusion_flutter_charts/charts.dart';
-
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import '../models/fade_animation.dart';
 
 class HomePage extends ConsumerStatefulWidget {
@@ -28,15 +28,108 @@ class _HomePageState extends ConsumerState<HomePage> {
           child: Column(
             children: [
               Container(
-                height: MediaQuery.of(context).size.height * 0.55,
-                decoration: BoxDecoration(color: Colors.grey),
-                child: SfCircularChart(series: <CircularSeries>[
-                  // Renders radial bar chart
-                  RadialBarSeries<ChartData, String>(
-                      dataSource: chartData,
-                      xValueMapper: (ChartData data, _) => data.x,
-                      yValueMapper: (ChartData data, _) => data.y)
-                ]),
+                height: MediaQuery.of(context).size.height * 0.4.sp,
+                decoration: BoxDecoration(
+                  color: Colors.green.shade50,
+                  borderRadius: const BorderRadius.only(
+                    bottomLeft: Radius.circular(25),
+                    bottomRight: Radius.circular(25),
+                  ),
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    SfCircularChart(
+                      series: <CircularSeries>[
+                        // Renders radial bar chart
+                        RadialBarSeries<ChartData, String>(
+                            useSeriesColor: true,
+                            radius: '120',
+                            trackOpacity: 0.3,
+                            cornerStyle: CornerStyle.bothCurve,
+                            dataSource: chartData,
+                            xValueMapper: (ChartData data, _) => data.x,
+                            yValueMapper: (ChartData data, _) => data.y)
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        Container(
+                          child: Column(
+                            children: [
+                              Text("data"),
+                              Text("56"),
+                            ],
+                          ),
+                        ),
+                        Container(
+                          child: Column(
+                            children: [
+                              Text("data"),
+                              Text("56"),
+                            ],
+                          ),
+                        ),
+                        Container(
+                          child: Column(
+                            children: [
+                              Text("data"),
+                              Text("56"),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 10.sp,
+              ),
+              Text(
+                "Tracks",
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
+              ),
+              Container(
+                margin: EdgeInsets.all(10.sp),
+                width: MediaQuery.of(context).size.width * 0.8.sp,
+                height: MediaQuery.of(context).size.height * 0.05.sp,
+                decoration: BoxDecoration(
+                  color: Colors.lightGreenAccent.shade100,
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(40.sp),
+                  ),
+                ),
+                child: Center(child: Text("Daily Activity")),
+              ),
+              Container(
+                margin: EdgeInsets.all(10.sp),
+                width: MediaQuery.of(context).size.width * 0.8.sp,
+                height: MediaQuery.of(context).size.height * 0.05.sp,
+                decoration: BoxDecoration(
+                  color: Colors.lightGreenAccent.shade100,
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(40.sp),
+                  ),
+                ),
+                child: Center(child: Text("Daily Activity")),
+              ),
+              SizedBox(
+                height: 35.sp,
+                child: Divider(
+                  color: Colors.black,
+                  indent: 40.sp,
+                  endIndent: 40.sp,
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                child: Text(
+                  "Start Off-setting",
+                  style: TextStyle(
+                      fontSize: 18.sp, color: Colors.lightGreen.shade900),
+                ),
               ),
             ],
           ),

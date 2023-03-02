4984ac9dd8d3864f54f65fc9d4b80b428b74ebd2
Yash Satankar <yashsatankar21@gmail.com>
2023-03-02 10:30:20 +0530

    subscribe

--- lib/screens/subscribe.dart
+++ lib/screens/subscribe.dart
@@ -8,7 +8,10 @@ class SubscribePage extends StatefulWidget {
   State<SubscribePage> createState() => _SubscribePageState();
 }
 
+enum SingingCharacter { lafayette, jefferson }
+
 class _SubscribePageState extends State<SubscribePage> {
+  SingingCharacter? _character = SingingCharacter.lafayette;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -42,18 +45,99 @@ class _SubscribePageState extends State<SubscribePage> {
               ),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.center,
-                children: const [
-                  Text(
+                children: [
+                  const Text(
                     "I want to offset...",
                     style: TextStyle(
                       fontSize: 16,
                       fontWeight: FontWeight.bold,
                     ),
                   ),
+                  const SizedBox(
+                    height: 50,
+                  ),
+                  OptionWidget(
+                    character: _character,
+                    rupees: "Rs 400/month",
+                    text: "100% of my footprint",
+                  ),
+                  OptionWidget(
+                    character: _character,
+                    rupees: "Rs 500/month",
+                    text: "120% of my footprint",
+                  ),
+                  OptionWidget(
+                    character: _character,
+                    rupees: "Rs 800/month",
+                    text: "200% of my footprint",
+                  ),
+                  OptionWidget(
+                    character: _character,
+                    rupees: "",
+                    text: "Custom",
+                  ),
                 ],
               ),
             ),
           ],
+        ),
+      ),
+    );
+  }
+}
+
+class OptionWidget extends StatelessWidget {
+  final String text;
+  final String rupees;
+  const OptionWidget({
+    super.key,
+    required SingingCharacter? character,
+    required this.text,
+    required this.rupees,
+  }) : _character = character;
+
+  final SingingCharacter? _character;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 10),
+      decoration: const BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.all(
+          Radius.circular(18),
+        ),
+        border: Border(
+          bottom: BorderSide(
+            color: Colors.green,
+          ),
+          top: BorderSide(
+            color: Colors.green,
+          ),
+          left: BorderSide(
+            color: Colors.green,
+          ),
+          right: BorderSide(
+            color: Colors.green,
+          ),
+        ),
+      ),
+      child: ListTile(
+        onTap: () {},
+        contentPadding: const EdgeInsets.all(8),
+        tileColor: Colors.white,
+        trailing: Text(rupees),
+        title: Text(text),
+        leading: Radio<SingingCharacter>(
+          activeColor: Colors.green,
+          toggleable: true,
+          value: SingingCharacter.lafayette,
+          groupValue: _character,
+          onChanged: (SingingCharacter? value) {
+            // setState(() {
+            //   _character = value;
+            // });
+          },
         ),
       ),
     );

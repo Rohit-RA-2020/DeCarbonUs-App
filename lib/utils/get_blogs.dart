eb9df6ac66441c8a880f9bd90b650faf1e85be0b
Rohit Ranjan <ranjanrohit812@gmail.com>
2023-03-03 11:39:53 +0530

    Http migration

--- lib/utils/get_blogs.dart
+++ lib/utils/get_blogs.dart
@@ -1,23 +1,22 @@
-import 'dart:convert';
 import 'package:decarbonus/models/blogs_model.dart';
-import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-
+import 'package:http/http.dart' as http;
 import '../providers/provider.dart';
 
 void getBlogs(WidgetRef ref, BuildContext context) async {
-  var dio = Dio();
+  var client = http.Client();
   try {
-    await dio
-        .get('https://api-account-345807.el.r.appspot.com/blogs')
+    await client
+        .get(Uri.parse('https://api-account-345807.el.r.appspot.com/blogs'))
         .then((value) {
       if (value.statusCode == 200) {
-        final welcome = blogsModelFromJson(jsonEncode(value.data));
+        final welcome = blogsModelFromJson(value.body);
         ref.read(blogsProvider.notifier).state = welcome;
       }
     });
-  } catch (e) {
+  } catch (feelings) {
+    //print(feelings);
     ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
       content: Text('Cannot Load Blogs'),
     ));
